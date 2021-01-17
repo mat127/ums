1a529e488cd11cc77700f1ef37008696988ae65a
mat127 <petr.matousek@gsl.cz>
2021-01-17 17:06:09 +0100

    + gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const del = require('del');
+const uglify = require('gulp-uglify');
+const cleanCss = require('gulp-clean-css');
+const ghp = require('gulp-gh-pages');
+
+function clean() {
+ return del('build/**', {force:true});
+}
+
+function ghPages() {
+  return gulp.src('build/**/*')
+    .pipe(ghp());
+}
+
+function compressJs() {
+  return gulp.src('src/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/'));
+}
+
+function compressCss() {
+    return gulp.src('src/*.css')
+        .pipe(cleanCss())
+        .pipe(gulp.dest('build/'));
+}
+
+function copyHtml() {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build/'));
+}
+
+function copy() {
+    return gulp.src(['src/*.html','src/*.js','src/*.css'])
+        .pipe(gulp.dest('build/'));
+}
+
+exports.package = gulp.parallel(compressJs,compressCss,copyHtml);
+exports.copy = copy;
+exports.clean = clean;
+exports.clean = clean;
+exports.deploy = gulp.series(clean,copy,ghPages);
